8dbf614f6561da68525cd9bef6d3bff1e886e9e6
Kody-PC\Kody <kody.kriner@gmail.com>
2018-02-24 12:12:50 -0700

    Implementing DynamoDB

---
+++ Utilities/GameHelper.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Yahtzee.Utilities
+{
+    class GameHelper
+    {
+
+    }
+}
